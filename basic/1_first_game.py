ef90bd931d8593bb670d0d12989822352b6276c2
ghatraxion <agh.arrafif@gmail.com>
2025-10-09 14:07:50 +0700

    day 1 learning pygame

--- basic/1_first_game.py
+++ basic/1_first_game.py
@@ -7,9 +7,31 @@ screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("first game!")
 
 color_white = (255, 255, 255)
+color_black = (0, 0, 255)
+
+p
+
+x, y = 10, 15
+speed = 5
+
+clock = pygame.time.Clock()
+fps_rate = 120
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        y += speed
+    if keys[pygame.K_DOWN]:
+        y -= speed
+    if keys[pygame.K_RIGHT]:
+        x += speed
+    if keys[pygame.K_LEFT]:
+        x -= speed
+
+    
