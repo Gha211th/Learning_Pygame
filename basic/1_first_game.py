b3045107d07220a3f998ea61120fef502f90070a
ghatraxion <agh.arrafif@gmail.com>
2025-10-09 14:18:39 +0700

    day 1 learning pygame

--- basic/1_first_game.py
+++ basic/1_first_game.py
@@ -24,16 +24,17 @@ while True:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_UP]:
-        y += speed
-    if keys[pygame.K_DOWN]:
         y -= speed
+    if keys[pygame.K_DOWN]:
+        y += speed
     if keys[pygame.K_RIGHT]:
         x += speed
     if keys[pygame.K_LEFT]:
         x -= speed
 
-    screen.blit(player, (x, y))
     screen.fill(color_white)
-    
+    pygame.draw.rect(screen, color_black, (x, y, 50, 50))
+    pygame.display.flip()
+    clock.tick(fps_rate)
 
     
