c382d4d9d90cfbf272fd8e9320b98e0059465196
ghatraxion <agh.arrafif@gmail.com>
2025-10-10 09:28:33 +0700

    practice:)

---
+++ Practicing_by_myself/1_lest_code.py
@@ -0,0 +1,11 @@
+# i just practice my skill at pygame
+# without using ai A.K.A code by myself
+
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("pacticing my pygame skills")
+
