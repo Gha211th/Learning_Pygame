04bb311d0b71fb9b7aac2d68c0baac20d9174663
ghatraxion <agh.arrafif@gmail.com>
2025-10-14 13:03:51 +0700

    pygame

--- basic/2_secong_game_with_enemy.py
+++ basic/2_secong_game_with_enemy.py
@@ -5,3 +5,11 @@ pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("my second game with enemy")
+
+color_white = (255, 255, 255)
+color_black = (0, 0, 255)
+color_red = (255, 0, 0)
+
+x, y = 10, 15
+speed = 5
+
