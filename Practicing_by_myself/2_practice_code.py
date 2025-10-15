51a79d45f542a80c3c3ee43b52be72491fea8eeb
ghatraxion <agh.arrafif@gmail.com>
2025-10-15 08:03:05 +0700

    pygame

--- Practicing_by_myself/2_practice_code.py
+++ Practicing_by_myself/2_practice_code.py
@@ -10,3 +10,23 @@ color_white = (255, 255, 255)
 color_black = (0, 0, 255)
 color_red = (255, 0, 0)
 
+player_x = 100
+player_y = 300
+player_speed = 5
+player_size = 50
+
+enemy_x = 100
+enemy_y = 400
+enemy_speed = 5
+enemy_size = 50
+
+enemy_direct = 5
+
+clock = pygame.time.Clock()
+fps_target = 120
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
