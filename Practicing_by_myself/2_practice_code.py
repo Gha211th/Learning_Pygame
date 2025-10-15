bab62d7c59835dd37bb056f4a2ebd66a8c6011e4
ghatraxion <agh.arrafif@gmail.com>
2025-10-15 08:14:57 +0700

    pygame

--- Practicing_by_myself/2_practice_code.py
+++ Practicing_by_myself/2_practice_code.py
@@ -48,10 +48,14 @@ while True:
     if player_x + box_size > 500:
         player_x = 500 - box_size
     if player_y < 0:
-        player_x = 0
+        player_y = 0
     if player_y + box_size > 500:
         player_y = 500 - box_size
+
+    enemy_x += enemy_speed * enemy_direct
     
+    if enemy_x <= 0 or enemy_x + enemy_size >= 500:
+        enemy_direct *= -1
     screen.fill(color_white)
     pygame.draw.rect(screen, color_black, (player_x, player_y, player_size, player_size))
     pygame.draw.rect(screen, color_red, (enemy_x, enemy_y, enemy_size, enemy_size))
