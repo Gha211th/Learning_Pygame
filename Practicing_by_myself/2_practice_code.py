a3fe6f49431a1c547e2419f0d2da8742d9f5bcdc
ghatraxion <agh.arrafif@gmail.com>
2025-10-15 08:10:34 +0700

    pygame

--- Practicing_by_myself/2_practice_code.py
+++ Practicing_by_myself/2_practice_code.py
@@ -25,6 +25,8 @@ enemy_direct = 5
 clock = pygame.time.Clock()
 fps_target = 120
 
+box_size = 50
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,3 +39,22 @@ while True:
     if keys[pygame.K_DOWN]:
         player_y += player_speed
     if keys[pygame.K_RIGHT]:
+        player_x += player_speed
+    if keys[pygame.K_LEFT]:
+        player_x -= player_speed
+
+    if player_x < 0:
+        player_x = 0
+    if player_x + box_size > 500:
+        player_x = 500 - box_size
+    if player_y < 0:
+        player_x = 0
+    if player_y + box_size > 500:
+        player_y = 500 - box_size
+    
+    screen.fill(color_white)
+    pygame.draw.rect(screen, color_black, (player_x, player_y, player_size, player_size))
+    pygame.draw.rect(screen, color_red, (enemy_x, enemy_y, enemy_size, enemy_size))
+    pygame.display.flip()
+
+    clock.tick(fps_target)
