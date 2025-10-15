b2cb9bde4aa607aa0479046bd36d14acf3239876
ghatraxion <agh.arrafif@gmail.com>
2025-10-15 08:00:25 +0700

    pygame

---
+++ Practicing_by_myself/2_practice_code.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("practicing:)")
+
+color_white = (255, 255, 255)
+color_black = (0, 0, 255)
+color_red = (255, 0, 0)
+
